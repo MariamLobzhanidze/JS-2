7f4e6e3497fd3b07446f3b5b0fedc47f2650b1e2
MariamLobzhanidze <mariam.lobjanidze.mariam@gmail.com>
2024-04-28 18:44:52 -0700

    1-5 homeworks

--- script.js
+++ script.js
@@ -1,1 +1,54 @@
 'use strict'
+
+// homework_1
+
+let numberArray = [
+1977,
+1978,
+1998,
+2006,
+2024
+]
+console.log(1977+1978+1998+2006+2024)
+
+// homework_2
+
+let personArray = [
+    {
+      name: "Mariam",
+      age: 25,
+      address: "Tbilisi",
+    },
+    {
+        name: "David",
+        age: 32,
+        address: "Khashuri",
+    },
+    {
+        name: "Ani",
+        age: 23,
+        address: "Batumi",
+    },
+  ];
+
+  // homework_3
+
+  console.log("My name is")
+  console.log(personArray[0].name)
+  console.log("My age is")
+  console.log(personArray[0].age)
+  console.log("My address is")
+  console.log(personArray[0].address)
+ 
+
+  // homework_4
+
+  if (personArray[1].age < 19) {
+    console.log("I am a teenager");
+  }
+
+  // homework_5
+  let numbersArray = [
+    27, 14, 33, 72, 11
+    ]
+console.log(27, 14, 33, 72, 11)
